5454fb32234e2b7748f0529c536bef1c9c212a00
deguchi <deguchi37458@gmail.com>
2022-09-07 11:41:24 +0900

    create Burgermenu

--- components/Common/Header/Nav/index.tsx
+++ components/Common/Header/Nav/index.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 import Link from 'next/link'
 
 import styles from './index.module.scss'
 
 export default function Nav() {
+  const [navIsOpen, setNavIsOpen] = useState(false)
+  const toggleNav = () => {
+    setNavIsOpen((prev) => !prev)
+  }
+
   return (
-    <nav className={styles.globalNav}>
-      <ul>
+    <nav className={navIsOpen ? styles.open : styles.close}>
+      <button className={styles.btn} onClick={toggleNav}>MENU</button>
+      <ul className={styles.lists}>
         <li>
         <Link href="/">
           <a>Top</a>
