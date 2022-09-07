bf4d36a408bdf8cc0f46a08e723dea5489868d04
deguchi <deguchi37458@gmail.com>
2022-09-07 15:23:46 +0900

    create Accordion

--- components/About/History/index.tsx
+++ components/About/History/index.tsx
@@ -1,5 +1,7 @@
 import BreadCrumb from '@/components/Common/BreadCrumb'
 
+import Accordion from '@/components/Common/Accordion'
+
 export default function History(): JSX.Element {
   return (
     <>
@@ -19,6 +21,15 @@ export default function History(): JSX.Element {
         ]}
       />
       <h1>HistoryContents</h1>
+      <Accordion heading="1994">
+        <p>1994テキスト</p>
+      </Accordion>
+      <Accordion heading="1995">
+        <p>1995テキスト</p>
+      </Accordion>
+      <Accordion heading="1996">
+        <p>1996テキスト</p>
+      </Accordion>
     </>
   )
 }
