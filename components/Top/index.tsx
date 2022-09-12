5e9b3d1a37e0bf69c75b8c55c402b9172fc5d525
deguchi <deguchi37458@gmail.com>
2022-09-12 14:55:29 +0900

    update top

--- components/Top/index.tsx
+++ components/Top/index.tsx
@@ -1,7 +1,15 @@
+import styles from "./index.module.scss"
+
 export default function Top(): JSX.Element {
   return (
     <>
-      <h1>Top</h1>
+    <div className={styles.top}>
+      <h1 className={styles.title}>
+        <span>Hiroki Deguchi</span>
+        <span>Portfolio</span>
+        <span>2023</span>
+      </h1>
+    </div>
     </>
   )
 }
