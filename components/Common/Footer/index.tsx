bf87b4e2ebe21ce8f38a14f257df4fd9a6637c6d
deguchi <deguchi37458@gmail.com>
2022-09-13 16:05:15 +0900

    update Footer

--- components/Common/Footer/index.tsx
+++ components/Common/Footer/index.tsx
@@ -4,6 +4,13 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       Footer
+      <div className={styles.list}>
+        <ul>
+          <li><a href="">Twitter</a></li>
+          <li><a href="">Instagram</a></li>
+          <li><a href="">Blog</a></li>
+        </ul>
+      </div>
     </footer>
   )
 }
