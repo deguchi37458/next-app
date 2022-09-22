ff3c35f33830fa2d1530334dac5cfa7eb085118f
deguchi <deguchi37458@gmail.com>
2022-09-22 19:07:17 +0900

    update top gsap

--- components/Top/index.tsx
+++ components/Top/index.tsx
@@ -1,15 +1,48 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger"
+
 import styles from "./index.module.scss"
 
 export default function Top(): JSX.Element {
+  
+  if (typeof window === 'object') {
+    gsap.registerPlugin(ScrollTrigger);
+    const els = document.querySelectorAll<HTMLElement>('.color')
+    els.forEach(el => {
+      let color = el.dataset.color;
+      console.log(color);
+      
+      gsap.to('body', {
+        backgroundColor: color,
+        scrollTrigger: {
+          trigger: el,
+          start: 'top center',
+          end: 'bottom center',
+          toggleActions: 'play pause resume reset'
+        }
+      })
+    })
+  }
+
   return (
     <>
-    <div className={styles.top}>
-      <h1 className={styles.title}>
-        <span>Hiroki Deguchi</span>
-        <span>Portfolio</span>
-        <span>2023</span>
-      </h1>
-    </div>
+      <section className={`${styles.sec} color`} data-color="#a30130">
+        <h1 className={styles.title}>
+          <span>Hiroki Deguchi</span>
+          <span>Portfolio</span>
+          <span>2023</span>
+        </h1>
+      </section>
+      <section className={`${styles.sec} color`} data-color="#266677">
+        <h1 className={styles.title}>
+          About
+        </h1>
+      </section>
+      <section className={`${styles.sec} color`} data-color="#f6f7a7">
+        <h1 className={styles.title}>
+          Work
+        </h1>
+      </section>
     </>
   )
 }
